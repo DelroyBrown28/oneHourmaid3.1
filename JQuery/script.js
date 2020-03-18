452af2e0ddff524a528fa6842f32571fdafb60ad
Delroy Brown <delroy_jb@hotmail.co.uk>
2020-03-18 15:34:50 +0000

    Small info modals explaining where to click/type added to no of rooms page

--- JQuery/script.js
+++ JQuery/script.js
@@ -101,23 +101,46 @@ $(document).ready(function () {
 
 
 
-    //INCREMENTS ROOMS BY 1
+    //DISPLAYS tap to add MESSAGE WHEN HOVERED
+    $(".bedroomsButton").mouseenter(showTapToAddPopUp);
 
+    function showTapToAddPopUp() {
+        $(".tapToAddRoomPopUp").fadeIn();
+        $(".typeInBoxPopUp").fadeIn(800);
+    }
+    
+
+    $(".bedroomsButton").mouseleave(hideTapToAddPopUp);
+
+    function hideTapToAddPopUp() {
+        $(".tapToAddRoomPopUp").fadeOut(800);
+        $(".typeInBoxPopUp").fadeOut();
+    }
+
+
+
+
+
+
+
+
+    //INCREMENTS ROOMS BY 1
     let addRoom = document.querySelectorAll(".addRoom");
     let subtractRoom = document.querySelectorAll(".subtractRoom");
 
     addRoom.forEach((el) => {
-        el.addEventListener('click', (e) => {
-            let input = e.target.closest('.bedroomsButton,.bathroomsButton,.kitchensButton').querySelector(".roomsAmount");
+        el.addEventListener("click", (e) => {
+            let input = e.target.closest(".bedroomsButton,.bathroomsButton,.kitchensButton").querySelector(".roomsAmount");
             input.value = parseInt(input.value) + 1;
         });
     });
 
     subtractRoom.forEach((el) => {
-        el.addEventListener('click', (e) => {
-            let input = e.target.closest('.bedroomsButton,.bathroomsButton, .kitchensButton').querySelector(".roomsAmount");
+        el.addEventListener("click", (e) => {
+            let input = e.target.closest(".bedroomsButton,.bathroomsButton, .kitchensButton").querySelector(".roomsAmount");
             input.value = parseInt(input.value) - 1;
         });
     });
+
 
 });
