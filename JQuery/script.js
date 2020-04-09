6f6cf180304a3315fec6031ede76584318f26df1
Delroy Brown <delroy_jb@hotmail.co.uk>
2020-04-09 10:42:57 +0100

    Find cleaner page started // Google Maps API added

--- JQuery/script.js
+++ JQuery/script.js
@@ -165,5 +165,35 @@ $(document).ready(function () {
         });
     });
 
-
 });
+
+//GOOGLE MAPS API
+function initMap() {
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 10,
+        center: {
+            lat: 51.630770,
+            lng: -0.735230,
+        }
+    });
+   
+
+    var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    var locations = [{
+        lat: 51.630770,
+        lng: -0.735230,
+    }, ];
+
+    var markers = locations.map(function (locations, i) {
+        return new google.maps.Marker({
+            position: locations,
+            label: labels[i % labels.length]
+        });
+    });
+
+    var markerCluster = new MarkerClusterer(map, markers, {
+        imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'
+    });
+
+}
+
