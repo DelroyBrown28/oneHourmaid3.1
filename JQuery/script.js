4d2a036d3f45f1939b75dfc0c2b4f829f77f7de3
Delroy Brown <delroy_jb@hotmail.co.uk>
2020-04-09 19:02:29 +0100

    go back button added to map page

--- JQuery/script.js
+++ JQuery/script.js
@@ -17,134 +17,6 @@ $(document).ready(function () {
         $(".typeInBoxPopUp").fadeOut();
     }
 
-    //REMOVES FORM INPUTS THAT AREN'T IN FOCUS
-    //TO SAVE KEYBOARD POP UP FROM PUSHING ELEMETS
-    $("#nameField").focusin(function () {
-        $("#contactField").fadeOut();
-        $(".cleaningLocation_title").fadeOut();
-        $("#streetAddress").fadeOut();
-        $("#townCity").fadeOut();
-        $("#postcode").fadeOut();
-        $("#cleaning_description_field").fadeOut();
-        $(".findCleanerWrap").fadeOut();
-    });
-
-    $("#nameField").focusout(function () {
-        $("#contactField").fadeIn();
-        $(".cleaningLocation_title").fadeIn();
-        $("#streetAddress").fadeIn();
-        $("#townCity").fadeIn();
-        $("#postcode").fadeIn();
-        $("#cleaning_description_field").fadeIn();
-        $(".findCleanerWrap").fadeIn();
-    });
-
-    $("#contactField").focusin(function () {
-        $("#nameField").fadeOut();
-        $(".cleaningLocation_title").fadeOut();
-        $("#streetAddress").fadeOut();
-        $("#townCity").fadeOut();
-        $("#postcode").fadeOut();
-        $("#cleaning_description_field").fadeOut();
-        $(".findCleanerWrap").fadeOut();
-    });
-
-    $("#contactField").focusout(function () {
-        $("#nameField").fadeIn();
-        $(".cleaningLocation_title").fadeIn();
-        $("#streetAddress").fadeIn();
-        $("#townCity").fadeIn();
-        $("#postcode").fadeIn();
-        $("#cleaning_description_field").fadeIn();
-        $(".findCleanerWrap").fadeIn();
-    });
-
-    $("#streetAddress").focusin(function () {
-        $("#nameField").fadeOut();
-        $(".cleaningLocation_title").fadeOut();
-        $("#contactField").fadeOut();
-        $("#townCity").fadeOut();
-        $("#postcode").fadeOut();
-        $("#cleaning_description_field").fadeOut();
-        $(".findCleanerWrap").fadeOut();
-    });
-
-    $("#streetAddress").focusout(function () {
-        $("#nameField").fadeIn();
-        $(".cleaningLocation_title").fadeIn();
-        $("#contactField").fadeIn();
-        $("#townCity").fadeIn();
-        $("#postcode").fadeIn();
-        $("#cleaning_description_field").fadeIn();
-        $(".findCleanerWrap").fadeIn();
-    });
-
-    $("#townCity").focusin(function () {
-        $("#nameField").fadeOut();
-        $(".cleaningLocation_title").fadeOut();
-        $("#contactField").fadeOut();
-        $("#streetAddress").fadeOut();
-        $("#postcode").fadeOut();
-        $("#cleaning_description_field").fadeOut();
-        $(".findCleanerWrap").fadeOut();
-    });
-
-    $("#townCity").focusout(function () {
-        $("#nameField").fadeIn();
-        $(".cleaningLocation_title").fadeIn();
-        $("#contactField").fadeIn();
-        $("#streetAddress").fadeIn();
-        $("#townCity").fadeIn();
-        $("#cleaning_description_field").fadeIn();
-        $(".findCleanerWrap").fadeIn();
-    });
-
-    $("#postcode").focusin(function () {
-        $("#nameField").fadeOut();
-        $(".cleaningLocation_title").fadeOut();
-        $("#contactField").fadeOut();
-        $("#streetAddress").fadeOut();
-        $("#townCity").fadeOut();
-        $("#cleaning_description_field").fadeOut();
-        $(".findCleanerWrap").fadeOut();
-    });
-
-    $("#postcode").focusout(function () {
-        $("#nameField").fadeIn();
-        $(".cleaningLocation_title").fadeIn();
-        $("#contactField").fadeIn();
-        $("#streetAddress").fadeIn();
-        $("#townCity").fadeIn();
-        $("#cleaning_description_field").fadeIn();
-        $(".findCleanerWrap").fadeIn();
-    });
-
-    $("#cleaning_description_field").focusin(function () {
-        $(".descriptionBoxWrap").animate({
-            bottom: "40%",
-        }); //PUSHES TEXT BOX UP PTHE PAGE TO GIVE ROOM TO THE POP UP KEYBOARD
-        $("#nameField").fadeOut();
-        $(".cleaningLocation_title").fadeOut();
-        $("#contactField").fadeOut();
-        $("#streetAddress").fadeOut();
-        $("#townCity").fadeOut();
-        $("#postcode").fadeOut();
-        $(".findCleanerWrap").fadeOut();
-    });
-
-    $("#cleaning_description_field").focusout(function () {
-        $(".descriptionBoxWrap").animate({
-            bottom: "0%",
-        }); //BRINGS TEXT BOX BACK DOWN TO ORIGINAL STATE
-        $("#nameField").fadeIn();
-        $(".cleaningLocation_title").fadeIn();
-        $("#contactField").fadeIn();
-        $("#streetAddress").fadeIn();
-        $("#townCity").fadeIn();
-        $("#postcode").fadeIn();
-        $(".findCleanerWrap").fadeIn();
-    });
-
 
     //INCREMENTS ROOMS BY 1
     let addRoom = document.querySelectorAll(".addRoom");
@@ -171,7 +43,7 @@ $(document).ready(function () {
 function initMap() {
     var map = new google.maps.Map(document.getElementById("map"), {
         zoom: 15,
-        disableDefaultUI: true,
+        disableDefaultUI: true, //DISABLES GOOGLE MAPS DEFAULT FUNCTIONS
         center: {
             lat: 51.630770,
             lng: -0.735230,
